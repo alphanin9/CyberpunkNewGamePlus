8e014e7af09875e94ab2ffc1e4d85288ed8d3a56
not_alphanine <not_alphanine@not_alphanine.not_alphanine>
2024-05-05 04:52:06 +0300

    Bumped version number

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ RED4EXT_C_EXPORT void RED4EXT_CALL Query(RED4ext::PluginInfo* aInfo)
 {
 	aInfo->name = L"New Game+";
 	aInfo->author = L"not_alphanine";
-	aInfo->version = RED4EXT_SEMVER(0, 9, 2); // Set your version here.
+	aInfo->version = RED4EXT_SEMVER(0, 9, 3); // Set your version here.
 	aInfo->runtime = RED4EXT_RUNTIME_LATEST;
 	aInfo->sdk = RED4EXT_SDK_LATEST;
 }
