24244a2c52f5c7e7cd8929b4c5bc343b79ed12a7
alphanin9 <47785076+alphanin9@users.noreply.github.com>
2024-12-24 00:34:13 +0200

    Fresh Start integration preliminary fix

--- src/hooking/hooking.cpp
+++ src/hooking/hooking.cpp
@@ -4,9 +4,10 @@
 #include <context/context.hpp>
 
 #include <Shared/Hooks/HookManager.hpp>
-#include <Shared/Raw/GameDefinition/GameDefinition.hpp>
-#include <Shared/Raw/Ink/InkSystem.hpp>
 #include <Shared/Raw/Telemetry/Telemetry.hpp>
+#include <Shared/Raw/Quest/QuestsSystem.hpp>
+#include <Shared/Raw/CharacterCustomizationSystem/CharacterCustomizationSystem.hpp>
+#include <Shared/Raw/CharacterCustomizationState/CharacterCustomizationState.hpp>
 
 using namespace Red;
 
@@ -22,16 +23,60 @@ void OnLoadTelemetryFactMap(HashMap<std::uint32_t, CString>& aMap)
     aMap.Insert(FNV1a32("ngplus_standalone_q101_start"), "ngplus_standalone_q101_start");
 }
 } // namespace LoadFacts
+
+namespace CharacterCustomizationSystem
+{
+void OnNewGame(Red::game::ui::CharacterCustomizationSystem* aThis, void* aRdx)
+{
+    // FIX: Fresh Start integration breaking things
+    // Implement a more robust fix using FDB mechanics used for setting cp77_new_game
+    auto& spinlock = shared::raw::CharacterCustomizationSystem::Lock::Ref(aThis);
+
+    // Lock state
+    std::unique_lock lock(spinlock);
+
+    auto& state = shared::raw::CharacterCustomizationSystem::State::Ref(aThis);
+
+    if (!state)
+    {
+        PluginContext::Error("hooking::CharacterCustomizationSystem::OnNewGame, CC state == NULL, WTF?");
+        return;
+    }
+
+    // No point running anything if we're not starting new game
+    // This shouldn't persist in save, so it shouldn't be running lots of times
+    if (!shared::raw::CharacterCustomizationState::IsNewGame(state))
+    {
+        return;
+    }
+    
+    TweakDBID lifepathId{};
+
+    shared::raw::CharacterCustomizationState::GetLifePath(state, lifepathId);
+
+    if (lifepathId == "LifePaths.NewStart")
+    {
+        auto questsSystem = GetGameSystem<quest::QuestsSystem>();
+
+        shared::raw::QuestsSystem::FactsDB(questsSystem)->SetFact("ngplus_fresh_start_on", 1);
+    }
+}
+}
+
 bool InitializeHooking()
 {
     shared::hook::HookAfter<shared::raw::Telemetry::LoadFactMap>(&LoadFacts::OnLoadTelemetryFactMap)
         .OrDie("Failed to hook Telemetry::LoadFacts");
+
+    shared::hook::HookAfter<shared::raw::CharacterCustomizationSystem::OnNewGame>(
+        &CharacterCustomizationSystem::OnNewGame)
+        .OrDie("Failed to hook CharacterCustomizationSystem::OnNewGame");
 
     return true;
 }
 
 bool DetachHooking()
 {
-    return shared::hook::Unhook<shared::raw::Telemetry::LoadFactMap>();
+    return shared::hook::Unhook<shared::raw::Telemetry::LoadFactMap>() && shared::hook::Unhook<shared::raw::CharacterCustomizationSystem::OnNewGame>();
 }
 } // namespace hooking
